41acd6a0600a7fa40f7f36e3b62dda219aebe2a2
soncastle <soncastle1090@gmail.com>
2025-04-03 18:49:48 +0900

    Create SwaggerConfig.java

---
+++ src/main/java/com/shoppingmall/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.shoppingmall.config;
+
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+            .group("all")
+            .pathsToMatch("/**") // API 경로 설정
+            .build();
+    }
+
+}
